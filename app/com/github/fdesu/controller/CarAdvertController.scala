be276d6336e6ca150a3c8031b922f9fa48afc880
fdesu <merkel05@gmail.com>
2019-01-25 10:31:36 +0100

    Fix broken formatting

--- app/com/github/fdesu/controller/CarAdvertController.scala
+++ app/com/github/fdesu/controller/CarAdvertController.scala
@@ -3,8 +3,8 @@ package com.github.fdesu.controller
 import java.util.stream.Collectors
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.github.fdesu.data.repo.CarAdvertRepo
 import com.github.fdesu.controller.validation.{CarAdvertValidator, ValidationException}
+import com.github.fdesu.data.repo.CarAdvertRepo
 import javax.inject.{Inject, Singleton}
 import javax.persistence.EntityNotFoundException
 import play.api.Logger
@@ -17,104 +17,104 @@ class CarAdvertController @Inject()(cc: ControllerComponents,
                                     mapper: ObjectMapper,
                                     validator: CarAdvertValidator) extends AbstractController(cc) {
 
-  def allAdverts(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    try {
-      Ok(mapper.writeValueAsString(
-        repo.all.collect(Collectors.toList())
-      ))
-    } catch {
-      case e: Exception =>
-        Logger.error("Critical exception during all records export operation", e)
-        InternalServerError
+    def allAdverts(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+        try {
+            Ok(mapper.writeValueAsString(
+                repo.all.collect(Collectors.toList())
+            ))
+        } catch {
+            case e: Exception =>
+                Logger.error("Critical exception during all records export operation", e)
+                InternalServerError
+        }
     }
-  }
 
-  def getCarAdvertById(id: Long): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    try {
-      val advert = repo.findById(id)
+    def getCarAdvertById(id: Long): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+        try {
+            val advert = repo.findById(id)
 
-      if (advert == null) {
-        NotFound
-      } else {
-        Ok(Json.toJson(CarAdvertResource.fromAdvert(advert)))
-      }
-    } catch {
-      case e: Exception =>
-        Logger.error("Critical exception during searching by id", e)
-        InternalServerError
+            if (advert == null) {
+                NotFound
+            } else {
+                Ok(Json.toJson(CarAdvertResource.fromAdvert(advert)))
+            }
+        } catch {
+            case e: Exception =>
+                Logger.error("Critical exception during searching by id", e)
+                InternalServerError
+        }
     }
-  }
 
-  def addNewAdvert(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    try {
-      request.body.asJson.map {
-        mapped =>
-          val json: JsResult[CarAdvertResource] = Json.fromJson[CarAdvertResource](mapped)
-          json match {
-            case JsSuccess(advertResource: CarAdvertResource, path: JsPath) => handleNewAdvert(advertResource)
-            case e: JsError => BadRequest("Request validation error occurred")
-          }
-      }.getOrElse {
-        BadRequest("Expecting application/json request body")
-      }
-    } catch {
-      case e: Exception =>
-        Logger.error("Critical exception during searching by id", e)
-        InternalServerError
+    def addNewAdvert(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+        try {
+            request.body.asJson.map {
+                mapped =>
+                    val json: JsResult[CarAdvertResource] = Json.fromJson[CarAdvertResource](mapped)
+                    json match {
+                        case JsSuccess(advertResource: CarAdvertResource, path: JsPath) => handleNewAdvert(advertResource)
+                        case e: JsError => BadRequest("Request validation error occurred")
+                    }
+            }.getOrElse {
+                BadRequest("Expecting application/json request body")
+            }
+        } catch {
+            case e: Exception =>
+                Logger.error("Critical exception during searching by id", e)
+                InternalServerError
+        }
     }
-  }
 
-  def handleNewAdvert(advertResource: CarAdvertResource): Result = {
-    try {
-      val advert = CarAdvertResource.toAdvert(advertResource)
+    def handleNewAdvert(advertResource: CarAdvertResource): Result = {
+        try {
+            val advert = CarAdvertResource.toAdvert(advertResource)
 
-      validator validate advert
-      repo persist advert
+            validator validate advert
+            repo persist advert
 
-      Ok(Json.toJson(IdResponse(advert.getId)))
-    } catch {
-      case e: ValidationException =>
-        BadRequest(Json.toJson(mapper.writeValueAsString(
-          Json.toJson(BadResponse(e.getMessage))
-        )))
-      case e: Exception =>
-        Logger.error("Critical exception during the persist call", e)
-        InternalServerError
+            Ok(Json.toJson(IdResponse(advert.getId)))
+        } catch {
+            case e: ValidationException =>
+                BadRequest(Json.toJson(mapper.writeValueAsString(
+                    Json.toJson(BadResponse(e.getMessage))
+                )))
+            case e: Exception =>
+                Logger.error("Critical exception during the persist call", e)
+                InternalServerError
+        }
     }
-  }
 
-  def updateAdvert(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    try {
-      request.body.asJson.map {
-        mapped =>
-          val json: JsResult[CarAdvertResource] = Json.fromJson[CarAdvertResource](mapped)
-          json match {
-            case JsSuccess(advertResource: CarAdvertResource, path: JsPath) =>
-              repo update CarAdvertResource.toAdvert(advertResource)
-              Ok
+    def updateAdvert(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+        try {
+            request.body.asJson.map {
+                mapped =>
+                    val json: JsResult[CarAdvertResource] = Json.fromJson[CarAdvertResource](mapped)
+                    json match {
+                        case JsSuccess(advertResource: CarAdvertResource, path: JsPath) =>
+                            repo update CarAdvertResource.toAdvert(advertResource)
+                            Ok
 
-            case e: JsError => BadRequest("Request validation error occurred")
-          }
-      }.getOrElse {
-        BadRequest("Expecting application/json request body")
-      }
-    } catch {
-      case e: Exception =>
-        Logger.error("Critical exception during searching by id", e)
-        InternalServerError
+                        case e: JsError => BadRequest("Request validation error occurred")
+                    }
+            }.getOrElse {
+                BadRequest("Expecting application/json request body")
+            }
+        } catch {
+            case e: Exception =>
+                Logger.error("Critical exception during searching by id", e)
+                InternalServerError
+        }
     }
-  }
 
-  def removeAdvert(id: Long): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    try {
-      repo delete id
-      Ok
-    } catch {
-      case e: EntityNotFoundException => NotFound
-      case e: Exception =>
-        Logger.error("Critical exception during removal", e)
-        InternalServerError
+    def removeAdvert(id: Long): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+        try {
+            repo delete id
+            Ok
+        } catch {
+            case e: EntityNotFoundException => NotFound
+            case e: Exception =>
+                Logger.error("Critical exception during removal", e)
+                InternalServerError
+        }
     }
-  }
 
 }
--- app/com/github/fdesu/controller/CarAdvertResource.scala
+++ app/com/github/fdesu/controller/CarAdvertResource.scala
@@ -3,8 +3,8 @@ package com.github.fdesu.controller
 import java.time.LocalDate
 
 import com.github.fdesu.data.model.{CarAdvert, Fuel}
-import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 case class CarAdvertResource(id: Long, title: String, fuel: Fuel,
                              price: Int, isNew: Boolean, mileage: Int,
@@ -13,53 +13,55 @@ case class CarAdvertResource(id: Long, title: String, fuel: Fuel,
 
 object CarAdvertResource {
 
-  implicit val implicitWrites = new Writes[CarAdvertResource] {
-    def writes(ad: CarAdvertResource): JsValue = {
-      Json.obj(
-        "id" -> ad.id,
-        "title" -> ad.title,
-        "fuel" -> ad.fuel.toString,
-        "price" -> ad.price,
-        "new" -> ad.isNew,
-        "mileage" -> ad.mileage,
-        "registrationDate" -> ad.registrationDate
-      )
+    implicit val implicitWrites = new Writes[CarAdvertResource] {
+        def writes(ad: CarAdvertResource): JsValue = {
+            Json.obj(
+                "id" -> ad.id,
+                "title" -> ad.title,
+                "fuel" -> ad.fuel.toString,
+                "price" -> ad.price,
+                "new" -> ad.isNew,
+                "mileage" -> ad.mileage,
+                "registrationDate" -> ad.registrationDate
+            )
+        }
     }
-  }
 
-  implicit val implicitReads: Reads[CarAdvertResource] = (
-    (__ \ "id").read[Long] and
-      (__ \ "title").read[String] and
-      (__ \ "fuel").read[String].map(f => Fuel.valueOf(f)) and
-      (__ \ "price").read[Int] and
-      (__ \ "new").read[Boolean] and
-      (__ \ "mileage").read[Int] and
-      (__ \ "registrationDate").read[LocalDate]
-    ) (CarAdvertResource.apply _)
+    implicit val implicitReads: Reads[CarAdvertResource] = (
+        (__ \ "id").read[Long] and
+            (__ \ "title").read[String] and
+            (__ \ "fuel").read[String].map(f => Fuel.valueOf(f)) and
+            (__ \ "price").read[Int] and
+            (__ \ "new").read[Boolean] and
+            (__ \ "mileage").read[Int] and
+            (__ \ "registrationDate").read[LocalDate]
+        ) (CarAdvertResource.apply _)
 
-  def toAdvert(ad: CarAdvertResource): CarAdvert = {
-    new CarAdvert(0L, ad.title, ad.fuel, ad.price, ad.isNew, ad.mileage, ad.registrationDate)
-  }
+    def toAdvert(ad: CarAdvertResource): CarAdvert = {
+        new CarAdvert(0L, ad.title, ad.fuel, ad.price, ad.isNew, ad.mileage, ad.registrationDate)
+    }
 
-  def fromAdvert(ad: CarAdvert): CarAdvertResource = {
-    CarAdvertResource(
-      ad.getId, ad.getTitle, ad.getFuel,
-      ad.getPrice, ad.isNew, ad.getMileage, ad.getRegistrationDate
-    )
-  }
+    def fromAdvert(ad: CarAdvert): CarAdvertResource = {
+        CarAdvertResource(
+            ad.getId, ad.getTitle, ad.getFuel,
+            ad.getPrice, ad.isNew, ad.getMileage, ad.getRegistrationDate
+        )
+    }
 
 }
 
 case class IdResponse(id: Long) {}
+
 object IdResponse {
-  implicit val implicitWrites: Writes[IdResponse] = Json.writes[IdResponse]
-  implicit val implicitReads: Reads[IdResponse] = Json.reads[IdResponse]
+    implicit val implicitWrites: Writes[IdResponse] = Json.writes[IdResponse]
+    implicit val implicitReads: Reads[IdResponse] = Json.reads[IdResponse]
 }
 
 case class BadResponse(msg: String) {}
+
 object BadResponse {
-  implicit val implicitWrites: Writes[BadResponse] = Json.writes[BadResponse]
-  implicit val implicitReads: Reads[BadResponse] = Json.reads[BadResponse]
+    implicit val implicitWrites: Writes[BadResponse] = Json.writes[BadResponse]
+    implicit val implicitReads: Reads[BadResponse] = Json.reads[BadResponse]
 }
 
 
